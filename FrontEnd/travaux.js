3887e092d6f1cfc1941e1c955767e09882dee840
MaximeH2024 <mhouguet91@gmail.com>
2024-07-05 09:47:04 +0200

    création dynamique du menu catégorie

--- FrontEnd/travaux.js
+++ FrontEnd/travaux.js
@@ -10,6 +10,7 @@ const travaux = await reponse.json();
 
 console.log(travaux); 
 
+
 function genererTravaux(travaux) {
 
     for (let i = 0; i < travaux.length; i++){
@@ -33,3 +34,41 @@ function genererTravaux(travaux) {
 }
 
 genererTravaux(travaux);
+
+// Création du menu contenant les fiddérentes catégories
+
+const reponsecategories = await fetch("http://localhost:5678/api/categories");
+const categories = await reponsecategories.json();
+
+console.log (categories);
+
+function genererMenuCategorie(categories){
+
+    
+        const sectionMenu = document.querySelector("#portfolio");
+        const menuCategorie = document.createElement("form");
+        const selectMenu = document.createElement("select");
+        selectMenu.id = "choixmenu";
+        selectMenu.name = "categories";
+        selectMenu.size = categories.length;
+        selectMenu.multiple = true;
+        const labelMenu = document.createElement("label");
+        labelMenu.htmlFor = selectMenu.id
+        labelMenu.innerText = "Choisissez vos options :"
+        for (let i = 0; i < categories.length; i++) {
+        const optionMenu = document.createElement("option");
+        optionMenu.value = categories[i].name;
+        optionMenu.innerText = categories[i].name;
+        selectMenu.appendChild(optionMenu);
+        }
+
+        //rattachement
+        sectionMenu.appendChild(menuCategorie);
+        menuCategorie.appendChild(labelMenu);
+        menuCategorie.appendChild(selectMenu);
+        
+    
+}
+
+genererMenuCategorie(categories);
+
